945bccdf05da202186a71e35474576a6bbad593f
hvihvi <hugo.vinet@gmail.com>
2025-08-08 12:30:42 +0200

    chore: remove navigation to narrow down issue

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,51 @@
-import { Pressable, Text } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
-import { useSfx } from '@/hooks/sfx/useSfx';
-import { useTheme } from '@react-navigation/native';
-import { useRouter } from 'expo-router';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { ConfettiVideo } from "@/components/video/ConfettiVideo";
+import { useSfx } from "@/hooks/sfx/useSfx";
+import { useTheme } from "@react-navigation/native";
+import { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   const theme = useTheme();
-  const router = useRouter();
   const { play } = useSfx();
+  const [showConfetti, setShowConfetti] = useState(false);
 
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Pressable onPress={() => {
-        router.push('/video');
-      }}
-      onPressIn={() => {
-        play();
-      }}
-      >
-        <Text style={{ color: theme.colors.primary, fontSize: 20, fontWeight: '500' }}>Go to Video</Text>
-      </Pressable>
-      </SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      {showConfetti && <ConfettiVideo />}
+      <View style={styles.absCenter}>
+        <Pressable
+          onPress={() => {
+            setShowConfetti((prev) => !prev);
+          }}
+          onPressIn={() => {
+            play();
+          }}
+        >
+          <Text
+            style={{
+              color: theme.colors.primary,
+              fontSize: 20,
+              fontWeight: "500",
+            }}
+          >
+            Toggle Video
+          </Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
-
+const styles = StyleSheet.create({
+  absCenter: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
